61c2b5bb3f87a7186f263e98b7420269e0f80ffe
Leonardo Dalvi Alvarenga <leonardodalvi@outlook.com>
2022-02-28 22:28:07 -0300

    upload project

---
+++ plot1.R
@@ -0,0 +1,78 @@
+####################
+# Setting Language #
+####################
+
+# Set locale
+Sys.setlocale("LC_TIME", "English")
+
+#############################
+# Setting Working Directory #
+#############################
+
+# Import library rstudioapi
+library(rstudioapi)
+
+# Set working directory to source file location
+setwd(dirname(getActiveDocumentContext()$path))
+
+# Check updated working directory
+getwd()
+
+####################
+# Downloading Data #
+####################
+
+# Import library data.table
+library(data.table)
+
+# Download Dataset
+file_url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("exdata_data_household_power_consumption.zip")){
+    download.file(file_url,"exdata_data_household_power_consumption.zip", mode = "wb")
+    unzip("exdata_data_household_power_consumption.zip", exdir = getwd())
+}
+
+###############################
+# Reading and Converting Data #
+###############################
+
+# Reading list of all features
+features <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?")
+
+# Formatting date to Type Date
+features$Date <- as.Date(features$Date, "%d/%m/%Y")
+
+# Filtering data set from February 1, 2007 to February 2, 2007
+features <- subset(features,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Removing incomplete observation
+features <- features[complete.cases(features),]
+
+# Combining Date and Time columns
+dateTime <- paste(features$Date, features$Time)
+
+# Naming the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Removing Date and Time column
+features <- features[ ,!(names(features) %in% c("Date","Time"))]
+
+# Adding DateTime column
+features <- cbind(dateTime, features)
+
+# Formatting dateTime Column
+features$dateTime <- as.POSIXct(dateTime)
+
+############
+# Plotting #
+############
+
+# Graphical device PNG
+png(filename='plot1.png', width=480, height=480, units='px')
+
+# Plotting the histogram
+hist(features$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+# Closing device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,83 @@
+####################
+# Setting Language #
+####################
+
+# Set locale
+Sys.setlocale("LC_TIME", "English")
+
+#############################
+# Setting Working Directory #
+#############################
+
+# Import library rstudioapi
+library(rstudioapi)
+
+# Set working directory to source file location
+setwd(dirname(getActiveDocumentContext()$path))
+
+# Check updated working directory
+getwd()
+
+####################
+# Downloading Data #
+####################
+
+# Import library data.table
+library(data.table)
+
+# Download Dataset
+file_url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("exdata_data_household_power_consumption.zip")){
+    download.file(file_url,"exdata_data_household_power_consumption.zip", mode = "wb")
+    unzip("exdata_data_household_power_consumption.zip", exdir = getwd())
+}
+
+###############################
+# Reading and Converting Data #
+###############################
+
+# Reading list of all features
+features <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?")
+
+# Formatting date to Type Date
+features$Date <- as.Date(features$Date, "%d/%m/%Y")
+
+# Filtering data set from February 1, 2007 to February 2, 2007
+features <- subset(features,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# Removing incomplete observation
+features <- features[complete.cases(features),]
+
+# Combining Date and Time columns
+dateTime <- paste(features$Date, features$Time)
+
+# Naming the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+# Removing Date and Time column
+features <- features[ ,!(names(features) %in% c("Date","Time"))]
+
+# Adding DateTime column
+features <- cbind(dateTime, features)
+
+# Formatting dateTime Column
+features$dateTime <- as.POSIXct(dateTime)
+
+############
+# Plotting #
+############
+
+# Graphical device PNG
+png(filename='plot3.png', width=480, height=480, units='px')
+
+# Plotting line chart
+with(features, {
+    plot(Sub_metering_1~dateTime, type="l", ylab="Energy sub metering", col="black", xlab="")
+    lines(Sub_metering_2~dateTime,col="red")
+    lines(Sub_metering_3~dateTime,col="blue")
+})
+legend(x="topright", col=c("black", "red", "blue"), lty=c(1,1,1), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+# Closing device
+dev.off()
